08b4700846883524dc453b7981bf48aaeabd4113
Nicolas Sicolo <sicolo2208@gmail.com>
2023-09-13 15:05:06 -0300

    First commit

---
+++ src/Button.tsx
@@ -0,0 +1,10 @@
+export default function Button () {
+
+    let url = "https://www.google.com";
+
+    return (
+        <button className="bg-blue-500 text-white rounded px-4 py-2">
+            Click Me
+        </button>
+    )
+}
